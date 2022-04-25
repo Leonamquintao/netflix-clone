e00fffe889cbb2fb63dd6f7353a2de6233c92b98
Leonam Quintão <leonamquintao@gmail.com>
2022-04-25 15:31:42 -0300

    add listRow styles

--- src/components/MovieRow.tsx
+++ src/components/MovieRow.tsx
@@ -4,8 +4,21 @@ import './MovieRow.css';
 
 const MovieRow: React.FC<MovieRowProps> = ({ title, rowItems }) => {
   return (
-    <div className="row">
+    <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--listArea">
+        <div className="movieRow--list">
+          {rowItems.results.length > 0 &&
+            rowItems.results.map((item, key) => (
+              <div key={key} className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.original_name}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
