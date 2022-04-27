2bd36e3e2337d338111749f24e829b4ebfdd6b16
Leonam Quintão <leonamquintao@gmail.com>
2022-04-27 17:37:14 -0300

    added scroll buttons to lists

--- src/components/MovieRow.tsx
+++ src/components/MovieRow.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import { Item } from '../interfaces';
 import './MovieRow.css';
 
@@ -8,11 +10,60 @@ interface MovieRowProps {
 }
 
 const MovieRow: React.FC<MovieRowProps> = ({ title, rowItems }) => {
+  const PADDING_GIVEN = 60;
+  const ITEM_SIZE = 150;
+  const INITIAL_POSITION = -400;
+
+  const [scrollX, setScrollX] = useState<number>(INITIAL_POSITION);
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0) {
+      x = 0;
+    }
+    return setScrollX(x);
+  };
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    const listWidth = rowItems.results.length * ITEM_SIZE;
+
+    if (window.innerWidth - listWidth > x) {
+      x = window.innerWidth - listWidth - PADDING_GIVEN;
+    }
+    return setScrollX(x);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+
+      <div
+        aria-hidden
+        className="movieRow--left"
+        onClick={handleLeftArrow}
+        onKeyDown={handleLeftArrow}
+      >
+        <ArrowBackIosIcon style={{ fontSize: 50 }} />
+      </div>
+
+      <div
+        aria-hidden
+        className="movieRow--right"
+        onClick={handleRightArrow}
+        onKeyUp={handleRightArrow}
+      >
+        <ArrowForwardIosIcon style={{ fontSize: 50 }} />
+      </div>
+
       <div className="movieRow--listArea">
-        <div className="movieRow--list">
+        <div
+          className="movieRow--list"
+          style={{
+            marginLeft: scrollX,
+            width: rowItems.results.length * 150,
+          }}
+        >
           {rowItems.results.length > 0 &&
             rowItems.results.map((item, key) => (
               <div key={key} className="movieRow--item">
